f6d2eaae97787b50654123b04ad45ab27ac25ab9
Tulun <jaykiraly@gmail.com>
2016-01-31 20:28:29 -0800

    Added schema to toppings and pizzas.

--- pizza_store/db/schema.rb
+++ pizza_store/db/schema.rb
@@ -11,16 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201031724) do
+ActiveRecord::Schema.define(version: 20160201041115) do
 
   create_table "pizzas", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "pizza_id"
+    t.string   "name"
+    t.float    "price"
   end
 
   create_table "toppings", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "toppings_id"
+    t.string   "name"
   end
 
 end
